e6439bb589ab129fa14aee7248dd2153b23c6a72
AbdulazizWeb <68805578+AbdulazizWeb@users.noreply.github.com>
2025-12-23 10:08:15 +0500

    feat: enhance UI with new sidebar color variables and dark mode support
    feat: integrate App component and Toaster for notifications in main render
    
    fix: update LoginPage layout with background image and overlay
    
    feat: improve RegisterPage with error handling and form validation
    
    feat: add CashboxPage for invoice management with a table layout
    
    refactor: remove unused CashlowPage component
    
    feat: create ControlPanelPage with header actions setup
    
    feat: implement ReportsPage with header title and layout
    
    feat: add various asset files including background images and logos
    
    feat: implement JWT decoding utility for authentication
    
    feat: create token storage utility for managing authentication tokens
    
    feat: add Avatar component for user profile images
    
    feat: implement Sonner Toaster for notifications
    
    feat: create reusable Table components for consistent UI

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import { RouterProvider } from "react-router";
 import { router } from "./app/providers/router";
 import { Provider } from "react-redux";
 import { store } from "./app/providers/store/AppStore";
+import App from "./app/App";
+import { Toaster } from "./shared/ui/shadcn/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App>
+        <RouterProvider router={router} />
+        <Toaster position="top-right" expand={true} richColors theme="light" />
+      </App>
     </Provider>
   </StrictMode>
 );
